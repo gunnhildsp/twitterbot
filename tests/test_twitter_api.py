d6fe60fa14db3af3ab3024eb0c6fdd285b03e273
Gunnhild Svandal Presthus <gunnhild.h.p@gmail.com>
2018-11-10 00:08:43 +0100

    create analyse_tweets to tie in sentiment analysis

--- tests/test_twitter_api.py
+++ tests/test_twitter_api.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from unittest import mock
+
 from tweepy.api import API
 from tweepy import User
 
@@ -5,6 +8,7 @@ from twitterbot.twitter_api import (
     connect_to_api,
     get_followings,
     get_most_recent_status,
+    format_tweets,
 )
 
 
@@ -27,3 +31,42 @@ def test_get_status():
     user_id = 3098427092  # User that has tweets
     tweet = get_most_recent_status(api, user_id)
     assert tweet is not None
+
+
+def test_get_recent_tweets():
+    # TODO implement test
+    pass
+
+
+def test_format_tweets():
+    # Create mock tweet
+    tweet = mock.Mock()
+    tweet.created_at = datetime.now()
+    tweet.text = "RT @brianokken: Test &amp; Code 52: pyproject.toml : the future of Python packaging, with @brettsky https://t.co/hkHFqwKXjr"
+    tweet.user.id = 1234
+    tweet.user.screen_name = "1234_screen_name"
+    tweet.favorite_count = 1
+    tweet.retweet_count = 0
+    tweets = [tweet]
+    exp_cols = [
+        "created_at",
+        "text",
+        "user_id",
+        "user_screen_name",
+        "favorite_count",
+        "retweet_count",
+    ]
+    tweet_df = format_tweets(tweets)
+    assert set(tweet_df.columns) == set(exp_cols)
+    assert tweet_df.retweet_count.iloc[0] == 0
+    assert tweet_df.favorite_count.iloc[0] == 1
+
+
+def test_get_most_recent_status():
+    # TODO implement test
+    pass
+
+
+def test_analyse_tweets():
+    # TODO implement test
+    pass
--- twitterbot/twitter_api.py
+++ twitterbot/twitter_api.py
@@ -1,9 +1,13 @@
-from datetime import datetime
+import datetime
 import logging
 import os
 
 from dotenv import load_dotenv, find_dotenv
+import numpy as np
+import pandas as pd
 import tweepy
+
+from twitterbot.sentiment_analysis import text_sentiment
 
 load_dotenv(find_dotenv())
 consumer_token = os.environ.get("API_KEY")
@@ -37,11 +41,62 @@ def connect_to_api(
     return api
 
 
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            time.sleep(15 * 60)
+
+
 def get_followings(api):
     friends = []
-    for friend in tweepy.Cursor(api.friends).items():
+    for friend in limit_handled(tweepy.Cursor(api.friends).items()):
         friends.append(friend)
     return api.friends_ids(), friends
+
+
+def get_recent_tweets(api, user_id, time):
+    max_tweets = 50  # maximum number of tweets to get from timeline
+    tweets = api.user_timeline(user_id, count=max_tweets)
+    # Return tweets newer than time and not comments
+    tweets = [
+        t for t in tweets if t.created_at > time and t.in_reply_to_status_id is None
+    ]
+    return tweets
+
+
+def format_tweets(tweets):
+    if not tweets:
+        return None
+    else:
+        cols = [
+            "created_at",
+            "text",
+            "user_id",
+            "user_screen_name",
+            "favorite_count",
+            "retweet_count",
+        ]
+        tweet_df = pd.DataFrame(columns=cols)
+        df_tmp_list = []
+        for tweet in tweets:
+            df_tmp = pd.DataFrame(
+                data=[
+                    [
+                        tweet.created_at,
+                        tweet.text,
+                        tweet.user.id,
+                        tweet.user.screen_name,
+                        tweet.favorite_count,
+                        tweet.retweet_count,
+                    ]
+                ],
+                columns=cols,
+            )
+            df_tmp_list.append(df_tmp)
+        tweet_df = tweet_df.append(df_tmp_list, sort=False)
+    return tweet_df
 
 
 def get_most_recent_status(api, user_id):
@@ -92,7 +147,7 @@ def unfollow_users_with_old_posts(maximum_age_days):
             api.destroy_friendship(user_id)
         else:
             tweet_time = tweet.created_at
-            duration = datetime.now() - tweet_time
+            duration = datetime.datetime.now() - tweet_time
             if duration.days > maximum_age_days:
                 logger.info(
                     f'Unfollowing user "{tweet.user.name}" as the newest post is {duration.days} days old'
@@ -100,15 +155,30 @@ def unfollow_users_with_old_posts(maximum_age_days):
                 api.destroy_friendship(user_id)
 
 
-if __name__ == "__main__":
-    tweet_age_days = 365
-    unfollow_users_with_old_posts(tweet_age_days)
+def analyse_tweets(tweet_df):
+    tweet_df["subjectivity"] = np.nan
+    tweet_df["polarity"] = np.nan
+    for row in tweet_df.itertuples():
+        sentiment_tuple = text_sentiment(row.text)
+        tweet_df.loc[row.Index, "polarity"] = sentiment_tuple.polarity
+        tweet_df.loc[row.Index, "subjectivity"] = sentiment_tuple.subjectivity
+    return tweet_df
 
+
+if __name__ == "__main__":
+    # Clean friends list
+    # tweet_age_days = 365
+    # unfollow_users_with_old_posts(tweet_age_days)
+
+    # Get tweets during recent week
+    time = datetime.datetime.now() - datetime.timedelta(days=7)
     api = connect_to_api()
     user_ids, _ = get_followings(api)
+    list_of_df = []
     for user_id in user_ids:
-        tweet = get_most_recent_status(api, user_id)
-        if tweet:
-            logger.info(
-                f"Tweeted by {tweet.user.screen_name} at {tweet.created_at}: \n {tweet.text}"
-            )
+        tweets = get_recent_tweets(api, user_id, time)
+        tweet_df = format_tweets(tweets)
+        list_of_df.append(tweet_df)
+    tweet_df = pd.concat(list_of_df, ignore_index=True)
+    tweets_classified = analyse_tweets(tweet_df)
+    print(tweets_classified["polarity"].max)
